1790e9aeb0532efd8ad578955a6a28079dd1be29
city-bridge <hironori0130@gmail.com>
2024-07-18 22:20:48 +0900

    fix output format

--- whisper_wrapper_client/__init__.py
+++ whisper_wrapper_client/__init__.py
@@ -1,4 +1,1 @@
 from .whisper_wrapper_client import WhisperWrapperClient
-from .sound_recode_process import SoundRecodeProcess
-from .sound_reocde_and_crop import SoundRecodeAndCrop
-
--- whisper_wrapper_client/__main__.py
+++ whisper_wrapper_client/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from .whisper_wrapper_client import WhisperWrapperClient
 
 
@@ -12,13 +13,13 @@ if len(sys.argv) >= 4:
     if cmd == 'stop':
         client.server_stop_request()
     elif cmd == 'trans_path':
-        print(client.transcribe_path(sys.argv[4]))   
+        print(json.dumps(client.transcribe_path(sys.argv[4])))   
     elif cmd == 'recg_path':
-        print(client.recognize_path(sys.argv[4]))
+        print(json.dumps(client.recognize_path(sys.argv[4])))
     elif cmd == 'recg_file':
-        print(client.recognize_file(sys.argv[4]))
+        print(json.dumps(client.recognize_file(sys.argv[4])))
     else:
-        print(client.transcribe_file(sys.argv[4]))
+        print(json.dumps(client.transcribe_file(sys.argv[4])))
 else:
     print("usage: [host] [port] [cmd] [args]")
 
